2ed21bb8b11c6f383441fae8e6d182c486cb01d6
Pablo <Pabloskys73@hotmail.es>
2023-12-22 20:31:44 +0100

    segundo intento

--- src/main/java/io/bootify/practica_spring_batch/PracticaSpringBatchApplication.java
+++ src/main/java/io/bootify/practica_spring_batch/PracticaSpringBatchApplication.java
@@ -1,14 +1,31 @@
 package io.bootify.practica_spring_batch;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class PracticaSpringBatchApplication {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
     public static void main(final String[] args) {
         SpringApplication.run(PracticaSpringBatchApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            jobLauncher.run(job, new JobParameters());
+        };
     }
 
 }
